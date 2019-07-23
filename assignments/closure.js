c9fab85b7ddd42a1addf53ed6bd529350c7d2301
_michelangeloxo <52550247+MADemery@users.noreply.github.com>
2019-07-22 22:19:15 -0700

    assignment answers partly complete

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,5 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+const plusone = () => {
+
+  let addone = 0;
+
+  return function(){
+
+    addone = addone + 1;
+    return addone; }
+  }; 
+
+  const plustwo = plusone();
+  console.log(plustwo());
+  console.log(plustwo());
 
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
@@ -8,10 +21,20 @@
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+count = 0;
+
+return () => {
+
+  count = count + 1;
+  return count;}
+
+
 };
+
 // Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+console.log(newCounter()); // 1
+console.log(newCounter()); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
